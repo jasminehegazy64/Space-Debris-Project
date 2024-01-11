e6939a71696175a67fe5384b30167cc8da58da02
Salma Baligh <97367130+salmabaligh123@users.noreply.github.com>
2024-01-11 15:23:10 +0000

    mainCode file

---
+++ mainCode.py
@@ -0,0 +1,11 @@
+import astropy
+from astropy.io import fits
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+import cv2
+from PIL import Image
+from google.colab.patches import cv2_imshow
+import os
+from scipy.optimize import curve_fit
+from google.colab import drive
