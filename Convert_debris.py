9ea479ea24761d68af71a7e1e484779d549254c7
jasmine <jasmine2004084@miuegypt.edu.eg>
2024-01-11 18:15:30 +0200

    convert_debris.py

---
+++ Convert_debris.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import cv2
+from astropy.io import fits
+
+
+def convert_fits_to_image(fits_filename, output_image_filename):
+    # Open the FITS file
+    with fits.open(fits_filename) as hdul:
+        # Get the data from the primary HDU (Header Data Unit)
+        data = hdul[0].data
+
+        # Noise reduction using Gaussian filter
+        data = cv2.GaussianBlur(data, (5, 5), 0)
+
+        # Sharpening using Laplacian filter
+        laplacian = cv2.Laplacian(data, cv2.CV_64F)
+        sharpened = data - 0.8 * laplacian
+
+        # Plot the data as an image without grid
+        plt.imshow(sharpened, cmap='gray')
+        plt.axis('off')  # Turn off the axis (including grid)
+
+        # Save the preprocessed image as a PNG file
+        plt.savefig(output_image_filename, bbox_inches='tight', pad_inches=0)
+        plt.close()
--- mainCode.py
+++ mainCode.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 import cv2
 from PIL import Image
-from google.colab.patches import cv2_imshow
+#from google.colab.patches import cv2_imshow
 import os
 from scipy.optimize import curve_fit
-from google.colab import drive
+#from google.colab import drive
