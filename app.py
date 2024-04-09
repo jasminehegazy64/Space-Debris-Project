79595f968c284176c3a5e48d150667ccc90afbd4
Salma Baligh <salmabaligh123@yahoo.com>
2024-04-09 19:56:44 +0200

    edits

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ USER_CREDENTIALS = {
 # def index():
 #     return render_template('index.html')
 
-
 # @app.route('/signup', methods=['POST'])
 # def signup():
 #     # Process form submission here
@@ -113,3 +112,5 @@ def messages():
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=9000)
+    
+    
